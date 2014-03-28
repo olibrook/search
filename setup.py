939b27f9de3cfc1a29a40fa61bab2f3f72e995d4
Oliver Brook <olibrook@gmail.com>
2014-03-28 11:54:23 +0000

    Fix packaging with setup.py.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thor",
+    version="0.1",
+    packages=find_packages(),
+    description="A wrapper for Google Appengine's search API",
+)
