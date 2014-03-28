b5f431b899cfc6df0b313a7f8a39cd6fab6b9bfc
Oliver Brook <olibrook@gmail.com>
2014-03-28 12:06:45 +0000

    The name 'thor' is already taken on PyPi. Change the egg name to 'potato-thor'. The package name should be changed in future too.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="thor",
+    name="potato-thor",
     version="0.1",
     packages=find_packages(),
     description="A wrapper for Google Appengine's search API",
